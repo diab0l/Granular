4540b5a4b5e6764950ed71646978208bf4ed522c
YuvalT <yuvaltze@gmail.com>
2015-04-04 18:58:30 +0300

    Media - ImageSource

---
+++ Granular.Presentation/Media/ImageSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Animation;
+using System.Windows.Markup;
+using System.Xaml;
+
+namespace System.Windows.Media
+{
+    public abstract class ImageSource : Animatable
+    {
+        public abstract IRenderImageSource RenderImageSource { get; }
+
+        public Size Size { get { return RenderImageSource != null ? RenderImageSource.Size : Size.Empty; } }
+    }
+}
