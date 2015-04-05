5c9ae85804027688b6ad22fd635a27b2576bccfd
YuvalT <yuvaltze@gmail.com>
2015-04-05 14:44:15 +0300

    Version 0.1.1

--- History.md
+++ History.md
@@ -1,5 +1,10 @@
 What's new
 ==========
 
+**0.1.1**
+- ISupportInitialize
+- ImageSource, BitmapSource, BitmapImage
+- Image
+
 **0.1.0**
 - Initial version
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -24,5 +24,5 @@
 
 [assembly: AssemblyVersion("0.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
