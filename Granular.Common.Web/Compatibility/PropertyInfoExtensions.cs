2c0c7c6fcc7296db533828de023d598211491cd4
YuvalT <yuvaltze@gmail.com>
2015-04-22 12:02:16 +0300

    Common Web - Compatibility - ParameterInfo

--- Granular.Common.Web/Compatibility/PropertyInfoExtensions.cs
+++ Granular.Common.Web/Compatibility/PropertyInfoExtensions.cs
@@ -5,23 +5,8 @@ using System.Text;
 
 namespace System.Reflection
 {
-    public class ParameterInfo
-    {
-        public Type ParameterType { get; private set; }
-
-        public ParameterInfo(Type parameterType)
-        {
-            this.ParameterType = parameterType;
-        }
-    }
-
     public static class PropertyInfoExtensions
     {
-        public static ParameterInfo[] GetIndexParameters(this PropertyInfo propertyInfo)
-        {
-            return propertyInfo.IndexParameterTypes.Select(type => new ParameterInfo(type)).ToArray();
-        }
-
         public static MethodInfo GetGetMethod(this PropertyInfo propertyInfo)
         {
             return propertyInfo.GetMethod;
