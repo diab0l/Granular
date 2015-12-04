7533059df8d270e0c0a806e1c10f8b288f533a02
YuvalT <yuvaltze@gmail.com>
2015-12-04 16:40:17 +0200

    Common - Extensions - Enumerable

---
+++ Granular.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Granular.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TSource> ConcatSingle<TSource>(this IEnumerable<TSource> source, TSource value)
+        {
+            return source.Concat(new[] { value });
+        }
+    }
+}
