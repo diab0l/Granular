e50f5ce1a30774e0a28fcae642430d921ed961cd
YuvalT <yuvaltze@gmail.com>
2015-12-05 22:00:12 +0200

    Adds support for three character color codes - String cast

--- Granular.Presentation/Media/Color.cs
+++ Granular.Presentation/Media/Color.cs
@@ -142,7 +142,7 @@ namespace System.Windows.Media
 
                 if (stringValue.StartsWith("#") && stringValue.Length == 4)
                 {
-                    return Color.FromUInt32(0xff000000 | Granular.Compatibility.Convert.ToUInt32(String.Format("{0}{0}{1}{1}{2}{2}", stringValue[1], stringValue[2], stringValue[3]), 16));
+                    return Color.FromUInt32(0xff000000 | Granular.Compatibility.Convert.ToUInt32(String.Format("{0}{0}{1}{1}{2}{2}", stringValue[1].ToString(), stringValue[2].ToString(), stringValue[3].ToString()), 16));
                 }
 
                 PropertyInfo propertyInfo = typeof(Colors).GetProperty(stringValue, BindingFlags.Static | BindingFlags.Public);
