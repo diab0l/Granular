96e4eed65c9f8f95ba935c789216175f8c0bfd6e
YuvalT <yuvaltze@gmail.com>
2016-06-25 12:11:11 +0300

    Controls - UniformGrid

---
+++ Granular.Presentation.Tests/Controls/Primitives/UniformGridTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Granular.Presentation.Tests.Controls.Primitives
+{
+    [TestClass]
+    public class UniformGridTest
+    {
+        [TestMethod]
+        public void UniformGridRowsTest()
+        {
+            FrameworkElement child1 = new FrameworkElement();
+            FrameworkElement child2 = new FrameworkElement();
+            FrameworkElement child3 = new FrameworkElement();
+
+            UniformGrid uniformGrid = new UniformGrid { Rows = 4 };
+
+            uniformGrid.Children.Add(child1);
+            uniformGrid.Children.Add(child2);
+            uniformGrid.Children.Add(child3);
+
+            uniformGrid.Measure(Size.Infinity);
+            uniformGrid.Arrange(new Rect(120, 120));
+
+            Assert.AreEqual(new Rect(0, 0, 120, 30), child1.VisualBounds);
+            Assert.AreEqual(new Rect(0, 30, 120, 30), child2.VisualBounds);
+            Assert.AreEqual(new Rect(0, 60, 120, 30), child3.VisualBounds);
+        }
+
+        [TestMethod]
+        public void UniformGridColumnsTest()
+        {
+            FrameworkElement child1 = new FrameworkElement();
+            FrameworkElement child2 = new FrameworkElement();
+            FrameworkElement child3 = new FrameworkElement();
+
+            UniformGrid uniformGrid = new UniformGrid { Columns = 4 };
+
+            uniformGrid.Children.Add(child1);
+            uniformGrid.Children.Add(child2);
+            uniformGrid.Children.Add(child3);
+
+            uniformGrid.Measure(Size.Infinity);
+            uniformGrid.Arrange(new Rect(120, 120));
+
+            Assert.AreEqual(new Rect(0, 0, 30, 120), child1.VisualBounds);
+            Assert.AreEqual(new Rect(30, 0, 30, 120), child2.VisualBounds);
+            Assert.AreEqual(new Rect(60, 0, 30, 120), child3.VisualBounds);
+        }
+
+        [TestMethod]
+        public void UniformGridDefaultLayoutTest()
+        {
+            FrameworkElement child1 = new FrameworkElement();
+            FrameworkElement child2 = new FrameworkElement();
+            FrameworkElement child3 = new FrameworkElement();
+
+            UniformGrid uniformGrid = new UniformGrid();
+
+            uniformGrid.Children.Add(child1);
+            uniformGrid.Children.Add(child2);
+            uniformGrid.Children.Add(child3);
+
+            uniformGrid.Measure(Size.Infinity);
+            uniformGrid.Arrange(new Rect(120, 120));
+
+            Assert.AreEqual(new Rect(0, 0, 60, 60), child1.VisualBounds);
+            Assert.AreEqual(new Rect(60, 0, 60, 60), child2.VisualBounds);
+            Assert.AreEqual(new Rect(0, 60, 60, 60), child3.VisualBounds);
+        }
+
+        [TestMethod]
+        public void UniformGridFirstColumnTest()
+        {
+            FrameworkElement child1 = new FrameworkElement();
+
+            UniformGrid uniformGrid = new UniformGrid { FirstColumn = 3 };
+
+            uniformGrid.Children.Add(child1);
+
+            uniformGrid.Measure(Size.Infinity);
+            uniformGrid.Arrange(new Rect(120, 120));
+
+            Assert.AreEqual(new Rect(60, 60, 60, 60), child1.VisualBounds);
+        }
+    }
+}
---
+++ Granular.Presentation/Controls/Primitives/UniformGrid.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Granular.Extensions;
+
+namespace System.Windows.Controls.Primitives
+{
+    public class UniformGrid : Panel
+    {
+        public static readonly DependencyProperty RowsProperty = DependencyProperty.Register("Rows", typeof(int), typeof(UniformGrid), new FrameworkPropertyMetadata(affectsMeasure: true));
+        public int Rows
+        {
+            get { return (int)GetValue(RowsProperty); }
+            set { SetValue(RowsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColumnsProperty = DependencyProperty.Register("Columns", typeof(int), typeof(UniformGrid), new FrameworkPropertyMetadata(affectsMeasure: true));
+        public int Columns
+        {
+            get { return (int)GetValue(ColumnsProperty); }
+            set { SetValue(ColumnsProperty, value); }
+        }
+
+        public static readonly DependencyProperty FirstColumnProperty = DependencyProperty.Register("FirstColumn", typeof(int), typeof(UniformGrid), new FrameworkPropertyMetadata(affectsMeasure: true));
+        public int FirstColumn
+        {
+            get { return (int)GetValue(FirstColumnProperty); }
+            set { SetValue(FirstColumnProperty, value); }
+        }
+
+        private int actualRows;
+        private int actualColumns;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            int cellsCount = FirstColumn + Children.Where(child => child.Visibility != Visibility.Collapsed).Count();
+
+            if (cellsCount == 0)
+            {
+                return Size.Zero;
+            }
+
+            actualRows = Rows > 0 ? Rows : (int)Math.Ceiling(Columns > 0 ? (double)cellsCount / Columns : Math.Sqrt(cellsCount)).Max(1);
+            actualColumns = Columns > 0 ? Columns : (int)Math.Ceiling((double)cellsCount / actualRows).Max(1);
+
+            Size availableCellSize = new Size(availableSize.Width / actualColumns, availableSize.Height / actualRows);
+            Size desiredCellSize = Size.Zero;
+
+            foreach (UIElement child in Children)
+            {
+                child.Measure(availableCellSize);
+                desiredCellSize = desiredCellSize.Max(child.DesiredSize);
+            }
+
+            return new Size(desiredCellSize.Width * actualColumns, desiredCellSize.Height * actualRows);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            double cellWidth = finalSize.Width / actualColumns;
+            double cellHeight = finalSize.Height / actualRows;
+
+            int cellIndex = FirstColumn;
+
+            foreach (UIElement child in Children)
+            {
+                int columnIndex = cellIndex % actualColumns;
+                int rowIndex = cellIndex / actualColumns;
+
+                child.Arrange(new Rect(columnIndex * cellWidth, rowIndex * cellHeight, cellWidth, cellHeight));
+
+                if (child.Visibility != Visibility.Collapsed)
+                {
+                    cellIndex++;
+                }
+            }
+
+            return finalSize;
+        }
+    }
+}
