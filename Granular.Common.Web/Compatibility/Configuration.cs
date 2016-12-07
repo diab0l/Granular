edb8fa2730bee4b450cbbfc37c208a9cafd62b8c
YuvalT <yuvaltze@gmail.com>
2016-12-07 10:14:59 +0200

    Compatibility - Empty namespaces

---
+++ Granular.Common.Web/Compatibility/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Configuration
+{
+    [Bridge.NonScriptable]
+    internal class Empty
+    {
+        //
+    }
+}
---
+++ Granular.Common.Web/Compatibility/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Data
+{
+    [Bridge.NonScriptable]
+    internal class Empty
+    {
+        //
+    }
+}
