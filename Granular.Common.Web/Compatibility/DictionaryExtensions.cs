169510236b9a111d6a368b39c564c798a5d3ce96
YuvalT <yuvaltze@gmail.com>
2016-12-29 21:02:47 +0200

    Compatibility - DictionaryExtensions - Clear

--- Granular.Common.Web/Compatibility/DictionaryExtensions.cs
+++ Granular.Common.Web/Compatibility/DictionaryExtensions.cs
@@ -7,6 +7,11 @@ namespace System.Collections.Generic
 {
     public static class DictionaryExtensions
     {
+        public static void Clear<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            ((Dictionary<TKey, TValue>)dictionary).Clear();
+        }
+
         public static IEnumerable<TKey> GetKeys<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
         {
             return dictionary.Keys;
