973c29d2ab31462e915c8746655a4137c54e5bb3
YuvalT <yuvaltze@gmail.com>
2017-01-09 00:03:42 +0200

    Bridge - Workaroud - Type full name - Cache

---
+++ Granular.Common.Web/Compatibility/Initialize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Granular.Common.Compatibility
+{
+    internal class Initialize
+    {
+        [Bridge.Init(Bridge.InitPosition.Before)]
+        static void DefineTypeFullNameCache()
+        {
+            /*@
+                Bridge.Reflection.resolveTypeFullName = Bridge.Reflection.getTypeFullName;
+
+                Bridge.Reflection.getTypeFullName = function (obj) {
+                    if (obj.$$fullname === undefined) {
+                        obj.$$fullname = Bridge.Reflection.resolveTypeFullName(obj);
+                    }
+
+                    return obj.$$fullname;
+                };
+            */
+        }
+    }
+}
